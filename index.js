44f7aa645966c5350224d4fc18fd99f6ab2c97b1
sajib9090 <nispap.devil.sajib@gmail.com>
2023-05-20 20:04:59 +0600

    sub category done

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+// mongodb
 
-
-
-// mongodb 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yc3f8jy.mongodb.net/?retryWrites=true&w=majority`;
-const uri = `mongodb+srv://toy-verse:cXguT5GBv6RyK0Kf@cluster0.yc3f8jy.mongodb.net/?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://toy-verse:lkL7TE42OLevs8ag@cluster0.yc3f8jy.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,38 +26,46 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    const database = client.db('ToysDB')
-    const haiku = database.collection('toys')
+    const database = client.db("ToysDB");
+    const haiku = database.collection("toys");
 
     // get/read
 
-    app.get('/toys', async(req, res) => {
-        const cursor = haiku.find()
-        const result = await cursor.toArray()
-        res.send(result)
+    app.get("/toys", async (req, res) => {
+      const cursor = haiku.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/toys/:subCategory', async (req, res) => {
+      const category = await haiku.find({category: req.params.subCategory}).toArray()
+      res.send(category)
+
     })
     
+
     // post
-    app.post('/toys', async(req, res) => {
-       const toy = req.body
-       const result = await haiku.insertOne(toy)
-       res.send(result)
-    //    console.log(toy);
-       
-    })
+    app.post("/toys", async (req, res) => {
+      const toy = req.body;
+      const result = await haiku.insertOne(toy);
+      res.send(result);
+      //    console.log(toy);
+    });
 
     //get
-    app.get('/toys/:id', async(req, res) => {
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-
-      const result = await haiku.findOne(query)
-      res.send(result)
+    app.get('/toys/:id', async (req, res) => {
+       const id = req.params.id;
+       const query = { _id: new ObjectId(id)}
+       const data = await haiku.findOne(query)
+       res.send(data)
     })
+
 
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -68,11 +73,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Toy-verse server is running')
-})
+app.get("/", (req, res) => {
+  res.send("Toy-verse server is running");
+});
 
 app.listen(port, () => {
-    console.log(`toy-verse is running on port, ${port}`);
-})
-
+  console.log(`toy-verse is running on port, ${port}`);
+});
