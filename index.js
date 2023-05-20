c83ef6ebf0f3864c2de4fe154731901bcfa33f24
sajib9090 <nispap.devil.sajib@gmail.com>
2023-05-21 01:43:29 +0600

    delted api created

--- index.js
+++ index.js
@@ -70,6 +70,15 @@ async function run() {
       //    console.log(toy);
     });
 
+    //delete
+
+    app.delete('/toys/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await haiku.deleteOne(query)
+      res.send(result)
+    })
+
     
 
 
